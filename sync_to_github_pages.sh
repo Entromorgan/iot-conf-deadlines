556a04bf889a4d66f6bf29483fe381ad1e8042aa
Borui Li <libr@emnets.org>
2021-01-05 13:53:44 +0800

    Initial commit

--- sync_to_github_pages.sh
+++ sync_to_github_pages.sh
@@ -5,6 +5,7 @@
 bundle install --jobs=3 --retry=3 --deployment
 bundle exec jekyll build --future
 bundle exec htmlproofer ./_site --only-4xx --check-favicon --check-html --url-ignore "/#.*/" --http-status-ignore "441"
-cd _site && git add .
-cd _site && git commit -m "Initial commit"
-cd _site && git push origin main
+cd _site
+git add .
+git commit -m "Initial commit"
+git push

5e37f2b6329435dfa7aaf36c7962a3047caf5e8d
Borui Li <libr@emnets.org>
2021-01-06 19:09:33 +0800

    Initial commit

--- sync_to_github_pages.sh
+++ sync_to_github_pages.sh
@@ -3,6 +3,7 @@
 # bundle --version
 # gem --version
 bundle install --jobs=3 --retry=3 --deployment
+# bundle exec jekyll serve
 bundle exec jekyll build --future
 bundle exec htmlproofer ./_site --only-4xx --check-favicon --check-html --url-ignore "/#.*/" --http-status-ignore "441"
 cd _site
